8ed19718dbdfc5b0a0779ee0ce4647354896879f
Jeff Dayton <ziydayton@hotmail.com>
2019-04-15 21:43:42 -0400

    Dayton Submission for Coursera R Programming week 3 assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
-}
+  ## Return a matrix that is the inverse of 'x'
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setInv <- function(solve) i <<- solve
+  getInv <- function() i
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
+} ##/makeCacheMatrix
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  i <- x$getInv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setInv(i)
+  i
+} ##/cacheSolve
